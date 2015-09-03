02e6a2aa58870808eef34b18c32c3cd2301464fe
artem <comdev@mail.ru>
2015-09-03 16:55:53 +0200

    --

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Rusakov on 03.09.2015.
+ */
+module.exports = AssertSoft;
+
+function AssertSoft(){
+    this.errors = [];
+
+    this.equal = function(firstVal, lastVal){
+        if(firstVal == lastVal){
+            return true;
+        }
+        this.errors.push('ќжидалось '+firstVal+', равно '+lastVal);
+        console.log(this.errors);
+        return false;
+    };
+}
